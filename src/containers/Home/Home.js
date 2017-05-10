05af437ca7d72e4a3c65af87e0c61b193f7e7ecd
Andy Tsai <andy@ketingdeMacbook-Pro.local>
2017-05-11 01:12:18 +0800

    finished css variables

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,22 +3,64 @@ import React, { Component } from 'react';
 import './Home.less';
 
 export default class Home extends Component {
+  state = {
+    spacing: 10,
+    blur: 10,
+    base: '#ffc600',
+  }
+
+  onChange = (key, value) => {
+    this.setState({
+      [key]: value,
+    });
+  }
+
   render() {
+    const { spacing, blur, base } = this.state;
+
+    const spanStyle = {
+      color: base,
+    };
+
+    const imageStyle = {
+      padding: `${spacing}px`,
+      filter: `blur(${blur}px)`,
+      background: base,
+    };
+
     return (
       <div>
-        <h2>Upload CSS Variables with <span>JS</span></h2>
+        <h2>Upload CSS Variables with
+          <span style={spanStyle}>JS</span>
+        </h2>
         <div className="controls">
           <label htmlFor="spacing">Spacing:</label>
-          <input type="range" name="spacing" min="10" max="200" value="10" />
+          <input
+            type="range"
+            min="10"
+            max="200"
+            value={spacing}
+            onChange={e => this.onChange('spacing', e.target.value)}
+          />
 
           <label htmlFor="spacing">Blur:</label>
-          <input type="range" name="blur" min="0" max="25" value="10" />
+          <input
+            type="range"
+            min="0"
+            max="25"
+            value={blur}
+            onChange={e => this.onChange('blur', e.target.value)}
+          />
 
           <label htmlFor="base">Base Color:</label>
-          <input type="color" name="base" value="#ffc600" />
+          <input
+            type="color"
+            value={base}
+            onChange={e => this.onChange('base', e.target.value)}
+          />
         </div>
 
-        <img alt="Background" src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
+        <img style={imageStyle} alt="Background" src="https://source.unsplash.com/7bwQXzbF6KE/800x500" />
       </div>
     );
   }
